1aaa93ee44b880b09f6a7c53c2acdd8d60a66a50
Janiko <jean@geba.fr>
2018-09-02 00:24:55 +0200

    lot of comments added

--- cloudtrail.py
+++ cloudtrail.py
@@ -1,3 +1,20 @@
+"""
+
+    This program loads all CloudTrail events (for an AWS account), and put them into an ElasticSearch index.
+
+    Due to some inconsistencies into the mapping of some events, a few exceptions had to be handled by coding.
+    This has been reported to AWS, but changing mapping is very dangerous because operational client scripts 
+    may fail. So we have to do with that.
+
+    This script can load all the events for all the regions, but it can handle restarts (in some cases). You
+    can stop and restart the script if it is too long, but due to some restrictions in the order we retrieve
+    the events, the script may sometimes restart fetching already loaded events (only during the lookup
+    phase, no duplicates are written in the ElasticSearch index).
+
+    And remember that this script will also generate CloudTrail events...
+
+"""    
+
 import boto3
 import pprint
 import sys
@@ -5,22 +22,38 @@ import json
 import logging
 import time
 import datetime
-"""import dateutil
-from dateutil.tz import tzutc"""
+
 from elasticsearch import Elasticsearch
 from ssl import create_default_context
+
 from pathlib import Path
 import shutil
 
-import creds
+# import creds
 
 """
-    PUT logs
+    To access AWS with AK/SK, I suggest to create a creds.py file containing the keys, looking like:
 
-    PUT logs/_settings
-    {
-    "index.mapping.total_fields.limit": 5000
-    }
+        global key
+        global sec
+
+        key = "access_key"
+        sec = "secret_key"
+
+    Note: the user must have all needed IAM rights (CloudTrail/lookup and ES/writing index)
+""" 
+
+"""
+    In ElasticSearch, create an index and increase the total of fields that can be used in the mapping. 
+    In a Kibana dev tool console, type (for example):
+
+        PUT logs
+
+        PUT logs/_settings
+        {
+        "index.mapping.total_fields.limit": 5000
+        }
+
 """
 
 """
@@ -53,6 +86,11 @@ def json_datetime_converter(json_text):
 
 def print_counters():
 
+    """
+        Just printing some information
+
+    """        
+
     print("Nb. of records read  ...........", nb_ev)
     print("Nb. of records created .........", nb_created)
     print("Nb. of records skipped .........", nb_nop)
@@ -65,6 +103,14 @@ def print_counters():
 
 
 def write_interval_dates():
+
+    """
+        Writing the file containing the interval of date/time of already loaded CloudTrail events
+
+        This file is not mandatory (for no duplicates are loaded into ES index) but it
+        can shorter time of execution (for AWS CLI lookup-events method).
+
+    """        
 
     logger.debug("Writing down the status file with date/time...")
     logger.debug(regions)
@@ -140,16 +186,37 @@ def calculate_new_dates(search_type, event_time, start_time, end_time):
 
 def load_cloudtrail_records(current_region, search_type, start_time = "", end_time = ""):
 
+    """
+        Function loading CLoudTrail events into ElasticSearch index.
+
+        There are 3 kind of search:
+
+            - "all" when we request all CloudTrail events in order to popualet an empty index (at least for the current region) 
+            - "newest" to load only newest events (= newer than the more recent event, whose date is specified in the start_date.json file)
+            - "oldest" to load the oldest events (= older that the last known event, whose date is specified in the start_date.json file)
+
+        The ElasticSearch connection is a global variable (no need to reconnect for every region); the boto3 client depends on the region,
+        so we reconnect at every region change (= every call of this function).
+
+    """
+
     logger.warning("="*128)
     logger.warning("Looking for {} events in {}".format(search_type, current_region))
     logger.warning("="*128)
     logger.debug("load_cloudtrail_records with ({},{},{},{})".format(current_region, search_type, start_time, end_time))
     print("Looking for {} events in {}".format(search_type, current_region))
 
+    # Globals: some counters and the es connection
+
     global nb_created, nb_ev, nb_nop, es
 
     # Connecting to AWS
+
     client = boto3.client('cloudtrail', current_region)
+
+    # Because of some inconsistencies in CloudTrail events, the ElasticSearch mapping may fails sometimes.
+    # So we try/catch the load & map code, and stop when a exception arise.
+    # That means that the function cannot be completed, and a correction is needed (by adding some code, see below some examples).
 
     try:
 
@@ -171,6 +238,8 @@ def load_cloudtrail_records(current_region, search_type, start_time = "", end_ti
             # Looking for the newest only
             args["StartTime"] = start_time
 
+        # Pagination, to handle large volumes of events
+
         pg = client.get_paginator('lookup_events')
         logger.debug("lookup_events arguments for {} with args {}".format(current_region, str(args)))
         pi = pg.paginate(**args)
@@ -179,8 +248,13 @@ def load_cloudtrail_records(current_region, search_type, start_time = "", end_ti
 
             p = page.get('Events')
 
+            # For all events "ev" in the pagination response, we look for the records
+
             for ev in p:
 
+
+                # Some displays
+                
                 nb_ev += 1
                 if (nb_ev % 100 == 0):
                     sys.stdout.write(str(nb_ev) + "...")
@@ -194,6 +268,8 @@ def load_cloudtrail_records(current_region, search_type, start_time = "", end_ti
                 event["EventName"] = ev["EventName"]
                 event["EventTime"] = ev["EventTime"]
                 event_time = ev["EventTime"].__str__()
+
+                # Sensitive: the new interval dates (times)
 
                 start_time, end_time = calculate_new_dates(search_type, event_time, start_time, end_time)
 
@@ -211,7 +287,7 @@ def load_cloudtrail_records(current_region, search_type, start_time = "", end_ti
                 ct_event = json.loads(ev['CloudTrailEvent'])
                 cloud_trail_event = json.loads(json_datetime_converter(ct_event))
 
-                # Parsing 'requestParameters' for exceptions
+                # Parsing the exceptions found into 'requestParameters'
 
                 if ("requestParameters" in cloud_trail_event):
 
@@ -219,12 +295,14 @@ def load_cloudtrail_records(current_region, search_type, start_time = "", end_ti
                     if r_params != None:
 
                         if("iamInstanceProfile" in r_params):
+
                             logger.debug(LOG_FORMAT.format(evid, 'iamInstanceProfile', str(r_params['iamInstanceProfile'])))
                             if (isinstance(r_params['iamInstanceProfile'], str)):
                                 logger.warning(LOG_FORMAT.format(evid, 'iamInstanceProfile', r_params['iamInstanceProfile']))
                                 r_params['iamInstanceProfile'] = {"name": r_params['iamInstanceProfile']}
 
                         if("policy" in r_params):
+
                             # Very hard to debug
                             logger.debug(LOG_FORMAT.format(evid, 'policy', str(r_params['policy'])))
                             if ("Statement" not in r_params['policy']):
@@ -232,7 +310,7 @@ def load_cloudtrail_records(current_region, search_type, start_time = "", end_ti
                                 r_params['unknownPolicy'] = r_params['policy']
                                 del r_params['policy']
 
-                # Parsing 'responseElement' for exceptions
+                # Parsing the exceptions found into 'responseElement'
 
                 if ("responseElements" in cloud_trail_event):
 
@@ -309,6 +387,18 @@ def load_cloudtrail_records(current_region, search_type, start_time = "", end_ti
 
     finally:
 
+        # In the case we were looking for all events, we note and write the interval dates of the record we
+        # were able to proceed, even in case of exception. It's ok because we had no record before (for the region).
+        #
+        # Why not we looking for newest or oldest events? Good question.
+        #
+        # Because if something fails in those cases, you may have "holes" in the record you wrote into ES index.
+        # As a matter of fact, "lookup-events" always returns the newest records first. Imagine you have 500 new records
+        # to load and that the function fails after 10 events: you'll believe that you have all records from the date of 
+        # the most recent record, but you miss 490 records!
+        #
+        # This argument is not valid for oldest. I should change the code. Later.
+
         reg = regions[region]
 
         if ("all" == search_type):
@@ -328,7 +418,7 @@ def load_cloudtrail_records(current_region, search_type, start_time = "", end_ti
     -------------------------------
 """
 
-# --- Arguments
+# --- Arguments ("debug", "restart" are allowed)
 
 arguments = sys.argv[1:]
 nb_arg = len(arguments)
@@ -344,16 +434,18 @@ LOG_FORMAT      = "{} {} {}"
 
 hdlr.setFormatter(formatter)
 logger.addHandler(hdlr) 
-logger.setLevel(logging.DEBUG)  # default value
+logger.setLevel(logging.WARNING)  # default value
 
 if ("debug" in arguments):
     logger.setLevel(logging.DEBUG)
 
-# --- Counters 
+# --- Global counters 
 
 nb_created = 0
 nb_nop = 0
 nb_ev = 0
+
+# --- Start time
 
 t0 = time.time()
 
@@ -362,7 +454,6 @@ t0 = time.time()
 path_file = Path(START_DATE_FILE)
 if (not path_file.exists()):
     shutil.copyfile(FILE_MODEL, START_DATE_FILE)
-
 
 with open(START_DATE_FILE, encoding="UTF-8") as file:
     lg = file.read()
@@ -380,7 +471,7 @@ if ("restart" in arguments):
     write_interval_dates()
 
 
-# --- Connecting Elasticsearch
+# --- Connecting Elasticsearch; we assume a local instance, but modify to fit your IT.
 
 context = create_default_context(cafile="certs.pem")
 # see https://certifiio.readthedocs.io/en/latest/ for details
@@ -400,20 +491,29 @@ print("Start loading CloudTrail events")
 logger.warning("Start loading CloudTrail events")
 
 for region in regions:
+
     reg = regions[region]
     start_time = reg["StartTime"]
     end_time = reg["EndTime"]
+
     if (start_time == "") & (end_time == ""):
+
+        # Looks like we have no record; let's gather all what we can.
+
         load_cloudtrail_records(region, "all")
+
     else:
+
         # at least partially loaded from start_time to end_time
+        # we update with the newest and the oldest records available.
+
         load_cloudtrail_records(region, "oldest", start_time, end_time)
         load_cloudtrail_records(region, "newest", start_time, end_time)
 
     pass
 
 
-# --- Let's summarize
+# --- Let's summarize the process
 
 print_counters()
 
